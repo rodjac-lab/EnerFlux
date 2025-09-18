214a22997ca17722ea9cdbcbfcd1ae0e117e6c59
rodjac-lab <rodolphe.jacson@gmail.com>
2025-09-18 22:36:58 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-   base: '/enerflux/',
+   base: '/EnerFlux/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
